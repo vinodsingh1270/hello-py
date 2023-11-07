e62edc4a5f4dd2ddfbe7cf40b30b20ea187adedd
Vinod Singh <33929391+vinodsingh1270@users.noreply.github.com>
2023-11-08 03:20:39 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,15 +1,102 @@
-from flask import Flask
+from flask import Flask, jsonify, abort, make_response, request
+from pip._vendor.appdirs import unicode
+from flask_httpauth import HTTPBasicAuth
+
+
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+#BASIC AUTH CHECK
+@auth.get_password
+def get_password(username):
+    if username == 'testing':
+        return 'python'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
 
 @app.route('/')
 def index():
-    name = "This is the route."
-    return name
+    return "Sample REST APIs Building!"
 
-@app.route('/hello')
-def hello():
-    name = "Hello world!"
-    return name
+#GET ALL THE TASKS WITH AUTH CHECK
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
+def get_tasks():
+    return jsonify({'tasks': tasks})
 
-if __name__ == "__main__":
-   
+#ADD NEW TASK
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+#SEARCH SPECIFIC TASK WITH ID
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+#UPDATE TASK WITH GIVEN ID
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+#DELETE TASK WITH GIVEN ID
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
+#ERROR HANDLING IN CASE OF 404
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+#RUN local development server
+if __name__ == '__main__':
+    app.run()
